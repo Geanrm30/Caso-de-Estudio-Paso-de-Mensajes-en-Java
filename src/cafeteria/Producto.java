ae0f92e7e50ad66415a45ca93a9fc1bbe56f9ba9
Geanrm30 <garodriguezm@uamv.edu.ni>
2026-04-29 20:41:40 -0600

    agregar clase Producto con atributos nombre y precio

---
+++ src/cafeteria/Producto.java
@@ -0,0 +1,43 @@
+package cafeteria;
+
+public class Producto {
+
+    // Atributos
+    private String nombre;
+    private double precio;
+
+    // Constructores
+    public Producto() {
+    }
+
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    // ToString
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
